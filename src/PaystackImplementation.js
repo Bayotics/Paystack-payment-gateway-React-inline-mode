8d73c6041bac1720ed8632a2da8a2c4287bb294d
bayoticstudios <abdullahishobaloju@gmail.com>
2023-04-08 17:38:08 +0100

    tailwind-applied

--- src/PaystackImplementation.js
+++ src/PaystackImplementation.js
@@ -20,27 +20,29 @@ const PaystackImplementation = () => {
     }
 
     return(
-        <div className="bg-gray-100 h-screen w-full pt-36">
-            <div className="w-96 h-96 ml-96 border shadow-2xl">
-            <form className= "grid mt-24 gap-4 grid-cols-2" id="paymentForm" onSubmit={payWithPaystack}>
+        <div className="h-screen w-full pt-36">
+            <div className="w-96 h-96 ml-96 bg-gray-100  border shadow-2xl">
+            <h1 className="text-xl font-bold mt-5">Paystack Gateway</h1>
+            <h4 className="text-sm mt-5">(Test Mode)</h4>
+            <form className= "grid mt-10 gap-4 grid-cols-2" id="paymentForm" onSubmit={payWithPaystack}>
                 <div className="form-group ">
                     <label htmlFor="email">Email Address</label>
-                    <input className="border rounded-md" type="email" value={email} onChange = {(e) => setEmail(e.target.value)} id="email-address" required />
+                    <input className="border h-8 w-44 rounded-md" type="email" value={email} onChange = {(e) => setEmail(e.target.value)} id="email-address" required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="amount">Amount</label>
-                    <input type="number" value={amount} onChange = {(e) => setAmount(e.target.value)} id="email-address" required />
+                    <input className="border h-8 w-44 rounded-md" type="number" value={amount} onChange = {(e) => setAmount(e.target.value)} id="email-address" required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="first-name">First Name</label>
-                    <input type="text" value={firstName} onChange = {(e) => setFirstName(e.target.value)} id="email-address" required />
+                    <input className="border h-8 w-44 rounded-md" type="text" value={firstName} onChange = {(e) => setFirstName(e.target.value)} id="email-address" required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="last-name">Last Name</label>
-                    <input type="text" value={lastName} onChange = {(e) => setLastName(e.target.value)} id="email-address" required />
+                    <input className="border h-8 w-44 rounded-md" type="text" value={lastName} onChange = {(e) => setLastName(e.target.value)} id="email-address" required />
                 </div>
-                <div className="form-submit">
-                    <button className = "ml-36 rounded-md border bg-gray w-24" type="submit"> Pay </button>
+                <div className="mt-12">
+                    <button className = "bg-green-500 h-12 w-48 ml-24 rounded-md border bg-gray w-24" type="submit"> Pay </button>
                 </div>
             </form>
             </div>
